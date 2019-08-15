0d8700d814ffdfe8f4d4d6fcdf25935155050b9f
William Eng <37854443+wileng@users.noreply.github.com>
2019-08-15 11:28:32 -0700

    fix broken tests

--- test/CoordinateServiceTest.js
+++ test/CoordinateServiceTest.js
@@ -15,7 +15,7 @@ describe('CoordinateService', () => {
                            new Coordinate(2, 1),
                            new Coordinate(1, 1)];
 
-        player.changeDirection(Direction.LEFT);
+        player.changeDirection(Direction.RIGHT);
         CoordinateService.movePlayer(player);
         let expectedSegments = [new Coordinate(6, 1),
                                 new Coordinate(5, 1),
@@ -71,7 +71,7 @@ describe('CoordinateService', () => {
                                 new Coordinate(1, 1)];
         assert.deepEqual(player.getSegments(), expectedSegments, 'Player did not move right and grow as expected');
 
-        player.changeDirection(Direction.UP);
+        player.changeDirection(Direction.DOWN);
         CoordinateService.movePlayer(player);
         expectedSegments = [new Coordinate(6, 2),
                             new Coordinate(6, 1),
